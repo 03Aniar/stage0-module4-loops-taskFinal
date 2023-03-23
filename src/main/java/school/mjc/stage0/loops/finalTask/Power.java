e7f62b247b6386cf286c2d6e8e000d8a041b4724
aniar <aniar.kaldybaiuly@nu.edu.kz>
2023-03-23 21:25:16 +0600

    v1

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,13 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int pow = 1;
+//increment the value of i after each iteration until the condition becomes false
+        for (int i = 1; i <= power; i++)
+//calculates power
+            pow = pow * numberToPrint;
+        System.out.println(pow);
+//returns power
     }
 
     public static void main(String[] args) {
